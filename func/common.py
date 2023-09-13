f056f9343d546df45512a54020aed4d6a807fe55
xiongyunhua <3288746512@qq.com>
2023-09-13 22:07:12 +0800

    新加树的先序遍历

--- func/common.py
+++ func/common.py
@@ -19,3 +19,21 @@ class Solution:
                 elif not stack:  #一次没入栈，就遇到}])
                     return False
         return not stack
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def preorderTraversal(self, root: [TreeNode]) -> list[int]:
+        res = []
+        def preorder(root: TreeNode):
+            if root == None:
+                return
+            res.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        preorder(root)
+        return res
